0b97270e9dbc1ee9e135dc2a4d6f6da0bc0356dc
Werner Hoier <werner.hoier@elennium.com>
2014-08-06 10:05:13 +0200

    Calculate iFrame height for page_edit according browser size

--- app/assets/javascripts/cms/page_toolbar.js
+++ app/assets/javascripts/cms/page_toolbar.js
@@ -11,3 +11,27 @@
 //= require cms/attachment_manager
 //= require bootstrap
 //= require cms/toolbar
+
+jQuery(function($){
+  // resize iframe according page height
+  if ($("#page_content").length > 0) {
+    if (window.addEventListener) {
+      window.addEventListener("load", resizeIFrameAccordingBodyHeight, false);
+      window.addEventListener("resize", resizeIFrameAccordingBodyHeight, false);
+    } else if (window.attachEvent) {
+      window.attachEvent("onload", resizeIFrameAccordingBodyHeight);
+      window.attachEvent("onresize", resizeIFrameAccordingBodyHeight);
+    }
+  }
+});
+
+function resizeIFrameAccordingBodyHeight() {
+  var D = document;
+  var documentHeight = Math.min(
+    Math.max(D.body.scrollHeight, D.documentElement.scrollHeight),
+    Math.max(D.body.offsetHeight, D.documentElement.offsetHeight),
+    Math.max(D.body.clientHeight, D.documentElement.clientHeight)
+  );
+  var iFrameHeight = documentHeight - $("#page_content_toolbar").offset().top - $("#page_content_toolbar").height();
+  $("#page_content").css("height", iFrameHeight + "px");
+}
